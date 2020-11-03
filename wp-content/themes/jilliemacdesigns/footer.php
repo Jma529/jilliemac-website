32ea57d1d97ac0b875a4eb2fad50cb44e7168556
Jill <jma529@gmail.com>
2020-11-03 15:59:41 +0800

    Add footer nav

--- wp-content/themes/jilliemacdesigns/footer.php
+++ wp-content/themes/jilliemacdesigns/footer.php
@@ -11,22 +11,21 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'jilliemacdesigns' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'jilliemacdesigns' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'jilliemacdesigns' ), 'jilliemacdesigns', '<a href="http://www.jilliemac.com">Jill Augustus</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+	<footer class="site-footer">
+		<nav class="footer-nav">
+			<?php
+				wp_nav_menu(
+					array(
+					'container'         => false,
+					'theme_location' => 'footer-menu',
+					'menu_class'        => 'footer-menu',
+					'depth'					 => 0
+				)
+			);
+			?>
+		</nav>
+	</footer>
+</div>
 
 <?php wp_footer(); ?>
 
