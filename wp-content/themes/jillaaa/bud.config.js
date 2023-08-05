b93c5dc6f893037bcb776ebf8ef3fe6eddb905ef
Jill Augustus <jma529@gmail.com>
2023-08-05 14:55:07 +1000

    Switch to sage-initial commit

---
+++ wp-content/themes/jillaaa/bud.config.js
@@ -0,0 +1,78 @@
+/**
+ * Compiler configuration
+ *
+ * @see {@link https://roots.io/docs/sage sage documentation}
+ * @see {@link https://bud.js.org/guides/configure bud.js configuration guide}
+ *
+ * @param {import('@roots/bud').Bud} app
+ */
+export default async (app) => {
+  /**
+   * Application assets & entrypoints
+   *
+   * @see {@link https://bud.js.org/docs/bud.entry}
+   * @see {@link https://bud.js.org/docs/bud.assets}
+   */
+  app
+    .entry('app', ['@scripts/app', '@styles/app.scss'])
+    .entry('editor', ['@scripts/editor', '@styles/editor'])
+
+    /**
+     * Directory contents to be included in the compilation
+     * @see {@link https://bud.js.org/docs/bud.assets/}
+     */
+    .assets(['images'])
+
+    /**
+     * Matched files trigger a page reload when modified
+     * @see {@link https://bud.js.org/docs/bud.watch/}
+     */
+    .watch(['resources/views', 'app'])
+
+    /**
+     * Proxy origin (`WP_HOME`)
+     * @see {@link https://bud.js.org/docs/bud.proxy/}
+     */
+    .proxy('http://jillaaa.local:8888')
+
+    /**
+     * Development origin
+     * @see {@link https://bud.js.org/docs/bud.serve/}
+     */
+    .serve('http://localhost:3000')
+
+
+  /**
+   * Set public path
+   *
+   * @see {@link https://bud.js.org/docs/bud.setPublicPath}
+   */
+  app.setPublicPath('/app/themes/sage/public/');
+
+  /**
+   * Generate WordPress `theme.json`
+   *
+   * @note This overwrites `theme.json` on every build.
+   *
+   * @see {@link https://bud.js.org/extensions/sage/theme.json}
+   * @see {@link https://developer.wordpress.org/block-editor/how-to-guides/themes/theme-json}
+   */
+  app.wpjson
+    .set('settings.color.custom', false)
+    .set('settings.color.customDuotone', false)
+    .set('settings.color.customGradient', false)
+    .set('settings.color.defaultDuotone', false)
+    .set('settings.color.defaultGradients', false)
+    .set('settings.color.defaultPalette', false)
+    .set('settings.color.duotone', [])
+    .set('settings.custom.spacing', {})
+    .set('settings.custom.typography.font-size', {})
+    .set('settings.custom.typography.line-height', {})
+    .set('settings.spacing.padding', true)
+    .set('settings.spacing.units', ['px', '%', 'em', 'rem', 'vw', 'vh'])
+    .set('settings.typography.customFontSize', false)
+    .useTailwindColors()
+    .useTailwindFontFamily()
+    .useTailwindFontSize()
+    .enable();
+};
---
+++ wp-content/themes/jillaaa/resources/views/front-page.blade.php
@@ -0,0 +1,3 @@
+@extends('layouts.app')
+
+<div class="w-full h-screen bg-cover" style="background-image: url('wp-content/themes/jillaaa/resources/images/bg-trees.jpeg')"></div>
