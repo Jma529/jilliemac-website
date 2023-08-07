35c660f86701687e6eb653af891da43fbf06c403
Jill Augustus <jma529@gmail.com>
2023-08-07 11:02:10 +1000

    Add background and intro

--- wp-content/themes/jillaaa/resources/views/front-page.blade.php
+++ wp-content/themes/jillaaa/resources/views/front-page.blade.php
@@ -1,3 +1,19 @@
 @extends('layouts.app')
 
-<div class="w-full h-screen bg-cover" style="background-image: url('wp-content/themes/jillaaa/resources/images/bg-trees.jpeg')"></div>
+<div class="w-full h-screen bg-cover bg-no-repeat grid grid-cols-2" style="background-image: url('wp-content/themes/jillaaa/resources/images/bg-trees_blur.jpg')">
+  <div class="col-start-1 col-span-full self-center flex flex-col justify-start items-start text-white">
+    <h1 class="text-xl">Hi! I'm Jill</h1>
+    <h2 class="text-lg"> I build websites and teach yoga.</h2>
+</div>
+<div class="scroll-button">
+        <a href="#about">
+          <span>
+          </span>
+          <span>
+          </span>
+          <span>
+          </span>
+          Scroll
+        </a>
+      </div>
+</div>
