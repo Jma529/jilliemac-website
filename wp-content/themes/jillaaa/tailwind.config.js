37e65d21f6064d2b8f802026c6aa3651ef460f9e
Jill Augustus <jma529@gmail.com>
2023-08-08 17:11:49 +1000

    Add fonts and reusable components

--- wp-content/themes/jillaaa/tailwind.config.js
+++ wp-content/themes/jillaaa/tailwind.config.js
@@ -3,7 +3,22 @@ const config = {
   content: ['./index.php', './app/**/*.php', './resources/**/*.{php,vue,js}'],
   theme: {
     extend: {
-      colors: {}, // Extend Tailwind's default colors
+      colors: {
+
+      }, // Extend Tailwind's default colors
+      spacing: {
+        'wrapper-lg': '8vw',
+        'wrapper-sm': '40px',
+        'section-y-lg': '120px',
+        'section-y-sm': '60px',
+      },
+      fontSize: {
+
+      },
+      fontFamily: {
+        'header': 'Ostrich Sans Heavy',
+        'body': 'titillium',
+      },
     },
   },
   plugins: [],
