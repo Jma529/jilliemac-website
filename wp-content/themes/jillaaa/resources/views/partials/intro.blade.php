adef4bb330fe465528187e7baac0528f5d093396
Jill Augustus <jma529@gmail.com>
2023-08-08 17:12:07 +1000

    Add timeline elements

---
+++ wp-content/themes/jillaaa/resources/views/partials/intro.blade.php
@@ -0,0 +1,16 @@
+<div class="w-full h-screen bg-cover bg-no-repeat lg:bg-center bg-[50%] grid grid-cols-2 " style="background-image: url('wp-content/themes/jillaaa/resources/images/bg-trees_blur.jpg')">
+  <div class="col-start-1 col-span-full self-center flex flex-col justify-start items-start text-white lg:w-1/2 w-3/4 lg:px-wrapper-lg px-wrapper-sm">
+    <h1 class="leading-none">Hi! I'm Jill</h1>
+    <h2 class="leading-none"> I build websites and teach yoga.</h2>
+</div>
+<div class="scroll-button">
+        <a href="#about">
+          <span>
+          </span>
+          <span>
+          </span>
+          <span>
+          </span>
+        </a>
+      </div>
+</div>
