772db34f0688605bb6f3c550296283d144bf5a13
Jill Augustus <jma529@gmail.com>
2023-08-22 16:29:21 +1000

    Add project cpt and partial

--- wp-content/themes/jillaaa/resources/views/partials/content-project.blade.php
+++ wp-content/themes/jillaaa/resources/views/partials/content-project.blade.php
@@ -1,13 +1,24 @@
-<article @php(post_class())>
-  @thumbnail
-    <h2 class="entry-title">
-      <a href="{{ get_permalink() }}">
-        {!! $title !!}
-      </a>
-    </h2>
+<?php
 
-    @include('partials.entry-meta')
-  <div class="entry-summary">
+  $args = array(
+  'post_type' => 'project',
+  'orderby' => 'menu_order',
+  'order' => 'ASC',
+);
+
+$project_loop = new WP_Query($args); 
+
+if ($project_loop->have_posts() ):
+
+while ($project_loop->have_posts() ): $project_loop->the_post(); ?>
+
+<a href="{{ get_permalink() }}">
+  <article class="text-stone lg:h-[50vh] bg-cover bg-no-repeat saturate-0 hover:saturate-100" @php(post_class()) style="background-image:url('@thumbnail('full', false)')">
+
+  <div class="entry-summary bg-charcoal bg-opacity-80 py-4 pl-4">
+    <p class="text-white">@title</p>
     @php(the_excerpt())
   </div>
-</article>
+  </article>
+</a>
+<?php endwhile; endif; ?>
--- wp-content/themes/jillaaa/resources/views/partials/portfolio.blade.php
+++ wp-content/themes/jillaaa/resources/views/partials/portfolio.blade.php
@@ -1,12 +1,7 @@
 <section class="bg-charcoal lg:px-wrapper-lg px-wrapper-sm lg:py-section-y-lg py-section-y-sm">
   <h2 class="mb-40 text-stone">Recent Work</h2>
   <div class="grid grid-cols-3 gap-8">
-    <div class="w-full">
       <!-- Start project loop -->
-  @while(have_posts()) @php(the_post())
     @include('partials.content-project')
-  @endwhile
-
-</div>
 </div>
 </section>
